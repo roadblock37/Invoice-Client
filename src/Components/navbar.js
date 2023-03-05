04cd41ead8b9fa412b5dad99bc190eb5ed83991d
roadblock37 <Roadblock37@gmail.com>
2023-03-05 15:52:33 -0500

    theme toggle icon now changing with theme

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,32 +1,31 @@
 import { ReactComponent as Logo } from "../assets/logo.svg";
 import { ReactComponent as Sun } from "../assets/icon-sun.svg";
 import { ReactComponent as Moon } from "../assets/icon-moon.svg";
-import { useDispatch, useSelector} from "react-redux";
-import {toggleLight, toggleDark} from "../Features/AllInvoices/allInvoiceSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  toggleLight,
+  toggleDark,
+} from "../Features/AllInvoices/allInvoiceSlice";
 
 const Navbar = () => {
   const themeState = useSelector((state) => state.allInvoices.theme);
-    
+
   const dispatch = useDispatch();
 
   const handleClick = () => {
-    if (themeState === 'light'){
-        dispatch(toggleDark())
-        console.log(themeState)
+    if (themeState === "light") {
+      dispatch(toggleDark());
+    } else {
+      dispatch(toggleLight());
     }
-    else {
-        dispatch(toggleLight());
-        console.log(themeState);
-    }
-}
+  };
 
   return (
     <span className="navbar">
       <Logo />
       <div className="toggle-container">
-        <button className="themeToggle" onClick={()=>
-        handleClick()}>
-          <Moon />
+        <button className="themeToggle" onClick={() => handleClick()}>
+          {themeState === "light" ? <Moon /> : <Sun />}
         </button>
         <div className="divider">
           <img
