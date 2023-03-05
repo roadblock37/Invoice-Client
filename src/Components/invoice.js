1ded1dd265750c2ff590408d0afbabe1d18cfa07
roadblock37 <Roadblock37@gmail.com>
2023-03-05 16:50:20 -0500

    partial styling of invoice

--- src/Components/invoice.js
+++ src/Components/invoice.js
@@ -13,15 +13,14 @@ const Invoice = ({
   total,
 }) => {
   return (
-  <article className="invoice">
-    <h2>{invoiceID}</h2>
-    <h5>{paymentDue}</h5>
-    <h5>{clientName}</h5>
-    <h2>{total}</h2>
-    <h5>{status}</h5>
-
-  </article>
-    );
+    <article className="invoice">
+      <h2>{`# ${invoiceID}`}</h2>
+      <h5>{paymentDue}</h5>
+      <h5>{clientName}</h5>
+      <h2>{total}</h2>
+      <h5>{status}</h5>
+    </article>
+  );
 };
 
 export default Invoice;
