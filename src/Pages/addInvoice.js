34fcf350a8ee8f751bb47fba292bc48f63c5798c
roadblock37 <Roadblock37@gmail.com>
2023-03-06 16:02:38 -0500

    basic addInvoice

--- src/Pages/addInvoice.js
+++ src/Pages/addInvoice.js
@@ -1,3 +1,12 @@
-const AddInvoice = () => {};
+import { useSelector, useDispatch } from "react-redux";
+import { setEditInvoice } from "../Features/Invoice/invoiceSlice";
+
+const AddInvoice = () => {
+  const { isLoading, statusOptions, status, isEditing, editInvoiceID } =
+    useSelector((store) => store.invoice);
+  const dispatch = useDispatch();
+
+  return <h1>Add Invoice</h1>;
+};
 
 export default AddInvoice;
