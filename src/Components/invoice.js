3a7721c2589fcab5dd52cf526e7040f300ea441c
roadblock37 <Roadblock37@gmail.com>
2023-03-05 19:16:51 -0500

    more invoice styling

--- src/Components/invoice.js
+++ src/Components/invoice.js
@@ -14,11 +14,17 @@ const Invoice = ({
 }) => {
   return (
     <article className="invoice">
-      <h2>{`# ${invoiceID}`}</h2>
-      <h5>{paymentDue}</h5>
-      <h5>{clientName}</h5>
-      <h2>{total}</h2>
-      <h5>{status}</h5>
+      <div className="id-container">
+        <h2>{`# ${invoiceID}`}</h2>
+        <h5>{clientName}</h5>
+      </div>
+      <div className="payment-container">
+        <div className="payment">
+          <h5>{paymentDue}</h5>
+          <h2>{total}</h2>
+        </div>
+        <h5>{status}</h5>
+      </div>
     </article>
   );
 };
