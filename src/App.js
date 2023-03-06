92c2c987668b239026a9760825296a0d1483f0b3
roadblock37 <Roadblock37@gmail.com>
2023-03-06 15:14:49 -0500

    rendering allInvoices correctly with router

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}></Route>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<AllInvoices />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
